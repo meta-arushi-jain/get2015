8324e924cdd5dd21cfcdf658f35ccc221173f5c1
meta-arushi-jain <arushi.jain@metacube.com>
2015-08-07 18:10:55 +0530

    Update AllTests.java

--- PF/Assignment1/AllTests.java
+++ PF/Assignment1/AllTests.java
@@ -1,3 +1,5 @@
+/*----checking for test suit cases of all classes
+*/
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -6,6 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ checkSort.class, convertBinaryToOctal.class, Fcfs.class,
 		JoinMerge.class, longestSequence.class, removeDuplicate.class })
-public class AllTests {
+public class AllTests 
+{
 
 }
