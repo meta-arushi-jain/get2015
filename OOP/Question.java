aad8fec4271bc139d953b987f70d2f28cdc6ce67
meta-arushi-jain <arushi.jain@metacube.com>
2015-08-14 15:57:30 +0530

    Create Question.java

---
+++ OOP/Question.java
@@ -0,0 +1,11 @@
+
+/**
+ * @author Arushi It is an interface for displaying Questions
+ */
+public interface Question {
+	public String singleSelect();
+
+	public String multipleSelect();
+
+	public String text();
+}
