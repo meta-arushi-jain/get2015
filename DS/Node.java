2a2ec0244374a00a125e1ccda278c4cda0604f9e
meta-arushi-jain <arushi.jain@metacube.com>
2015-09-04 14:46:55 +0530

    Create Node.java

---
+++ DS/Node.java
@@ -0,0 +1,25 @@
+//Class to create a binary tree
+/**
+ * @author Arushi --Node class for tree
+ *
+ */
+public class Node {
+	int data;
+	Node left;
+	Node right;
+
+	// constructor calling
+	Node() {
+		data = 0;
+		left = null;
+		right = null;
+	}
+
+	// Parameterized constructor calling
+	Node(int element) {
+		data = element;
+		left = null;
+		right = null;
+	}
+
+}
