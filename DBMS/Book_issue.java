499c437fdf3be533bc37dabaf9acb382e75bce4c
meta-arushi-jain <arushi.jain@metacube.com>
2015-09-16 18:43:33 +0530

    Create Book_issue.java

---
+++ DBMS/Book_issue.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+
+/**
+ * @author Arushi
+ * POJO class to store data of book_Issue
+ *
+ */
+public class Book_issue {
+	private String issueDate;
+	private String returDate;
+	private String accessionNoOfBookIssue;
+	private String memberIdOfBookIssue;
+	/**
+	 * @return the issueDate
+	 */
+	public String getIssueDate() {
+		return issueDate;
+	}
+	/**
+	 * @param issueDate the issueDate to set
+	 */
+	public void setIssueDate(String issueDate) {
+		this.issueDate = issueDate;
+	}
+	/**
+	 * @return the returDate
+	 */
+	public String getReturDate() {
+		return returDate;
+	}
+	/**
+	 * @param returDate the returDate to set
+	 */
+	public void setReturDate(String returDate) {
+		this.returDate = returDate;
+	}
+	/**
+	 * @return the accessionNoOfBookIssue
+	 */
+	public String getAccessionNoOfBookIssue() {
+		return accessionNoOfBookIssue;
+	}
+	/**
+	 * @param accessionNoOfBookIssue the accessionNoOfBookIssue to set
+	 */
+	public void setAccessionNoOfBookIssue(String accessionNoOfBookIssue) {
+		this.accessionNoOfBookIssue = accessionNoOfBookIssue;
+	}
+	/**
+	 * @return the memberIdOfBookIssue
+	 */
+	public String getMemberIdOfBookIssue() {
+		return memberIdOfBookIssue;
+	}
+	/**
+	 * @param memberIdOfBookIssue the memberIdOfBookIssue to set
+	 */
+	public void setMemberIdOfBookIssue(String memberIdOfBookIssue) {
+		this.memberIdOfBookIssue = memberIdOfBookIssue;
+	}
+	
+	
+}
